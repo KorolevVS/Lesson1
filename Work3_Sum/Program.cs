cf0314d32539d5029cc91260a17e4149bd0145ce
Vlad Korolev <korolevvsk@bk.ru>
2023-04-08 13:31:37 +0300

    commit work3 - скорректировали код суммы случайных чисел new Random().Next (A, B)

--- Work3_Sum/Program.cs
+++ Work3_Sum/Program.cs
@@ -8,12 +8,9 @@
 // пигем программу сложения 2 случайно сгенерированных чисел
 // new Random().Next (1, 31) - команда генерит случайные числа в диапазоне [min, max-1] или [min, max]
 int numberA = new Random().Next (1, 31); // 1 2 3 4 . . . . 30
-Console.WriteLine("первое число");
-Console.WriteLine(numberA);
+Console.WriteLine("первое число" + " " + "= " + numberA);
 int numberB = new Random().Next (1, 31); // 1 2 3 4 . . . . 30
-Console.WriteLine("второе число");
-Console.WriteLine(numberB);
+Console.WriteLine("второе число" + " " + "= " + numberB);
 int result = numberA + numberB;
-Console.WriteLine("сумма");
-Console.WriteLine(result);
+Console.WriteLine("сумма" + " " + "= " + result);
 
