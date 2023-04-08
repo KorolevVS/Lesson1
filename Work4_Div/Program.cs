b4786d1e196ab09da87b81f0371b6660bceb11bc
Vlad Korolev <korolevvsk@bk.ru>
2023-04-08 12:11:56 +0300

    commit work4 написали программу деления 2-х чисел

---
+++ Work4_Div/Program.cs
@@ -0,0 +1,8 @@
+﻿// пишем программу частного 2-х чисел (деление).
+// для целых чисел используем тип данных int.
+// для дробных чисел используем тип данных double.
+    
+double numberA = 12;
+double numberB = 5;
+double result = numberA / numberB;
+Console.WriteLine(result);
