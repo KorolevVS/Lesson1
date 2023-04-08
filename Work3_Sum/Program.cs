4575ad291f40cc3b96e94f984f4c55ce437d0e63
Vlad Korolev <korolevvsk@bk.ru>
2023-04-08 13:09:05 +0300

    commit work3 написали программу суммы случайных чисел new Random().Next (A, B)

--- Work3_Sum/Program.cs
+++ Work3_Sum/Program.cs
@@ -1,6 +1,19 @@
 ﻿// пишем программу сложения 2-х чисел
-int numberA = 31;
-int numberB = 15;
+// int numberA = 31;
+// int numberB = 5;
+// int result = numberA + numberB;
+// Console.WriteLine(result);
+
+
+// пигем программу сложения 2 случайно сгенерированных чисел
+// new Random().Next (1, 31) - команда генерит случайные числа в диапазоне [min, max-1] или [min, max]
+int numberA = new Random().Next (1, 31); // 1 2 3 4 . . . . 30
+Console.WriteLine("первое число");
+Console.WriteLine(numberA);
+int numberB = new Random().Next (1, 31); // 1 2 3 4 . . . . 30
+Console.WriteLine("второе число");
+Console.WriteLine(numberB);
 int result = numberA + numberB;
+Console.WriteLine("сумма");
 Console.WriteLine(result);
 
