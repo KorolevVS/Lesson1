08cc6f2245262aadc5d6f494e1b89f0d6e408754
Vlad Korolev <korolevvsk@bk.ru>
2023-04-08 14:33:38 +0300

    commit work5 - сделали программу приветствия с командой решающей проблему регистра - username.ToLower()

---
+++ Work5_Condition_If_Else/Program.cs
@@ -0,0 +1,18 @@
+﻿// программа приветствия пользователя (обычная/ особенная)
+// If
+// Else
+
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "vlad")
+{
+    Console.WriteLine("Здарова - неужели ты пришел!");
+}
+else
+{
+    Console.Write("Привет пользователь, ");
+    Console.WriteLine(username);
+}
+
+
