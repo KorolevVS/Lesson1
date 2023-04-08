289305c309bc127e8ab806b41a3b1b2dcc58e75e
Vlad Korolev <korolevvsk@bk.ru>
2023-04-08 14:46:42 +0300

    commit work6 - сделали программу определеия max числа

---
+++ Work6_Base/Program.cs
@@ -0,0 +1,17 @@
+﻿// решаем задачу с гирями
+int a = 1;
+int b = 5;
+int c = 12;
+int d = 9;
+int e = 6;
+int f = 10;
+int max = a;
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e;
+if (f > max ) max = f;
+Console.Write("max = ");
+Console.WriteLine(max);
+
